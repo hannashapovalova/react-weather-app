627a5d2acd70ceaf4b6f33f7d10d8619ec43d2a0
hannashapovalova <hannalova1991@gmail.com>
2022-10-03 22:32:21 +0100

    Made Website Template

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <main className="App-header">
+        <div className="container">
         <Weather />
+        </div>
       </main>
       <footer>
         This project was coded by{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,38 @@
+import './Weather.css';
+
 export default function Weather() {
     return (
      <div>
-        <p>Weather HERE!</p>
-       
+        <div className="row search-block">
+          Serach
+        </div>
+        <div className="row">
+          <div className="col-8 left-block">
+            <div className="row main-block">
+              Main block
+            </div>
+            <div className="row">
+              <div className="col-4 wind">
+                Wind
+              </div>
+              <div className="col-4 humidity">
+                Humidity
+              </div>
+              <div className="col-4 precipitation">
+                Precipitation
+              </div>
+            </div>
+          </div>
+          <div className="col-4 right-block">
+            <ul>
+              <li>Monday</li>
+              <li>Monday</li>
+              <li>Monday</li>
+              <li>Monday</li>
+              <li>Monday</li>
+            </ul>
+          </div>
+        </div>
      </div>
     );
 }
