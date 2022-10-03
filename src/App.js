11454da4efd0af9b2db10f6621e971a57eaaf97d
hannashapovalova <hannalova1991@gmail.com>
2022-10-03 22:11:59 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-header">
         <Weather />
-      </header>
+      </main>
+      <footer>
+        This project was coded by{" "}
+        <a 
+         href="https://www.linkedin.com/in/anna-shapovalova-63116b238/" 
+         target="_blank" 
+         rel="noreferrer">
+          Anna Shapovalova
+        </a> 
+        {" "}and is open-sourced on{" "}
+        <a
+         href="https://github.com/hannashapovalova/react-weather-app" 
+         target="_blank" 
+         rel="noreferrer">GitHub</a> 
+        {" "}and hosted on{" "}
+        <a
+         href="https://unique-kangaroo-bdf1ea.netlify.app/" 
+         target="_blank" 
+         rel="noreferrer">Netlify</a>
+        .
+      </footer>
     </div>
   );
 }
