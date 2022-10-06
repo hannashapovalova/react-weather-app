69c13d93d5266b81804fd4f90e22bc5d2f11e28a
hannashapovalova <hannalova1991@gmail.com>
2022-10-06 19:33:32 +0100

    Code refactoring

--- src/Weather.js
+++ src/Weather.js
@@ -4,118 +4,194 @@ import Icons from "./Icons";
 
 
 export default function Weather() {
-    return (
-     <div>
-        <div className="search-container">
-          <p className="search-title">
-            Type city here...
-          </p>
-          <input className="search-input" type="text" placeholder="Search"></input>
-        </div>
-        <div className="row">
-          <div className="col main-block left-block">
-            <div className="row weather-block">
-              <div className="col">
-                <div className="row">
-                  <h1 className="current-city">Funchal</h1>
-                  <h2 className="current-date">Tuesday 12:12</h2>
-                </div>
-                <div className="row">
-                  <div className="temperature-block">
-                  <div className="current-temperature">22 ° </div>
-                  <div className="unit">C | F</div>
-                  </div>
-                  <h2 className="current-description">Clear Sky</h2>
-                </div>
+  return (
+    <div>
+      <div className="search-container">
+        <p className="search-title">
+         Type city here...
+        </p>
+        <input
+         className="search-input" 
+         type="text" 
+         placeholder="Search">
+        </input>
+      </div>
+      <div className="row">
+        <div className="col main-block left-block">
+          <div className="row weather-block">
+            <div className="col">
+              <div className="row">
+                <h1 className="current-city">
+                  Funchal
+                </h1>
+                <h2 className="current-date">
+                  Tuesday 12:12
+                </h2>
               </div>
-              <div className="col current-icon">
-               <Icons icon="CLEAR_DAY" color="white" size="120" />
+              <div className="row">
+                <div className="temperature-block">
+                  <div className="current-temperature">
+                    22 ° 
+                  </div>
+                  <div className="unit">
+                    C | F
+                  </div>
+                </div>
+                  <h2 className="current-description">
+                    Clear Sky
+                  </h2>
               </div>
             </div>
-            <div className="row weather-conditions-block">
-              <div className="col wind">
-                <p>Wind</p>
-                <div className="row">
-                  <div className="col-6">
-                    <p className="conditions-value">4.15 m/h</p>
-                  </div>
-                  <div className="col-6">
-                     <Icons icon="WIND" color="white" size="65" />
-                  </div>
+            <div className="col current-icon">
+              <Icons
+               icon="CLEAR_DAY" 
+               color="white" 
+               size="120" />
+            </div>
+          </div>
+          <div className="row weather-conditions-block">
+            <div className="col wind">
+              <p>
+                Wind
+              </p>
+              <div className="row">
+                <div className="col-6">
+                  <p className="conditions-value">
+                    4.15 m/h
+                  </p>
+                </div>
+                <div className="col-6">
+                  <Icons
+                   icon="WIND" 
+                   color="white" 
+                   size="65" />
                 </div>
               </div>
-              <div className="col humidity">
-                <p>Humidity</p>
-                <div className="row">
-                  <div className="col-6">
-                    <p className="conditions-value">20%</p>
-                  </div>
-                  <div className="col-6">
-                    <Icons icon="FOG" color="white" size="65" />
-                  </div>
+            </div>
+            <div className="col humidity">
+              <p>
+                Humidity
+              </p>
+              <div className="row">
+                <div className="col-6">
+                  <p className="conditions-value">
+                    20%
+                  </p>
+                </div>
+                <div className="col-6">
+                  <Icons
+                   icon="FOG" 
+                   color="white" 
+                   size="65" />
                 </div>
               </div>
-              <div className="col precipitation">
-                <p>Precipitation</p>
-                <div className="row">
-                  <div className="col-6">
-                    <p className="conditions-value">2%</p>
-                  </div>
-                  <div className="col-6">
-                    <Icons icon="RAIN" color="white" size="65" />
-                  </div>
+            </div>
+            <div className="col precipitation">
+              <p>
+                Precipitation
+              </p>
+              <div className="row">
+                <div className="col-6">
+                  <p className="conditions-value">
+                    2%
+                  </p>
+                </div>
+                <div className="col-6">
+                  <Icons
+                   icon="RAIN" 
+                   color="white" 
+                   size="65" />
                 </div>
               </div>
             </div>
           </div>
-          <div className="col forecast-block">
-            <div className="row forecast-element">
-              <div className="col">
-                <h2 className="forecast-date">Monday</h2>
-                <p className="forecast-temperature">24 °C</p>
-              </div>
-              <div className="col forecast-icon">
-                <Icons icon="PARTLY_CLOUDY_DAY" color="white" size="65" />
-              </div>
+        </div>
+
+            {/* FORECAST BLOCK */}
+
+        <div className="col forecast-block">
+          <div className="row forecast-element">
+            <div className="col">
+              <h2 className="forecast-date">
+                Monday
+              </h2>
+              <p className="forecast-temperature">
+                24 °C
+              </p>
             </div>
-            <div className="row forecast-element">
-              <div className="col">
-                <h2 className="forecast-date">Monday</h2>
-                <p className="forecast-temperature">24 °C</p>
-              </div>
-              <div className="col forecast-icon">
-                <Icons icon="CLEAR_DAY" color="white" size="65" />
-              </div>
+            <div className="col forecast-icon">
+                <Icons
+                 icon="PARTLY_CLOUDY_DAY" 
+                 color="white" 
+                 size="65" />
             </div>
-            <div className="row forecast-element">
-              <div className="col">
-                <h2 className="forecast-date">Monday</h2>
-                <p className="forecast-temperature">24 °C</p>
-              </div>
-              <div className="col forecast-icon">
-                <Icons icon="CLOUDY" color="white" size="65" />
-              </div>
+          </div>
+          <div className="row forecast-element">
+            <div className="col">
+              <h2 className="forecast-date">
+                Monday
+              </h2>
+              <p className="forecast-temperature">
+                24 °C
+              </p>
             </div>
-            <div className="row forecast-element">
-              <div className="col">
-                <h2 className="forecast-date">Monday</h2>
-                <p className="forecast-temperature">24 °C</p>
-              </div>
-              <div className="col forecast-icon">
-                <Icons icon="RAIN" color="white" size="65" />
-              </div>
+            <div className="col forecast-icon">
+              <Icons
+               icon="CLEAR_DAY" 
+               color="white" 
+               size="65" />
             </div>
-            <div className="row forecast-element">
-              <div className="col">
-                <h2 className="forecast-date">Monday</h2>
-                <p className="forecast-temperature">24 °C</p>
-              </div>
-              <div className="col forecast-icon">
-                <Icons icon="PARTLY_CLOUDY_DAY" color="white" size="65" />
-              </div>
+          </div>
+          <div className="row forecast-element">
+            <div className="col">
+              <h2 className="forecast-date">
+              Monday
+              </h2>
+              <p className="forecast-temperature">
+                24 °C
+              </p>
+            </div>
+            <div className="col forecast-icon">
+              <Icons
+               icon="CLOUDY" 
+               color="white" 
+               size="65" />
+            </div>
+          </div>
+          <div className="row forecast-element">
+            <div className="col">
+              <h2 className="forecast-date">
+                Monday
+              </h2>
+              <p className="forecast-temperature">
+                24 °C
+              </p>
+            </div>
+            <div className="col forecast-icon">
+              <Icons
+               icon="RAIN" 
+               color="white" 
+               size="65" />
+            </div>
+          </div>
+          <div className="row forecast-element">
+            <div className="col">
+              <h2 className="forecast-date">
+                Monday
+              </h2>
+              <p className="forecast-temperature">
+                24 °C
+              </p>
+            </div>
+            <div className="col forecast-icon">
+              <Icons
+               icon="PARTLY_CLOUDY_DAY" 
+               color="white" 
+               size="65" />
             </div>
           </div>
         </div>
-     </div>
+      </div>
+    </div>
     );
 }
